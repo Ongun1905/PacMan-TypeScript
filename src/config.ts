a37fd677e45b8d271390fae307aaee330991b0d1
Ongun1905 <ongunkorkmaz@hotmail.com>
2022-06-05 17:13:23 +0200

    set debug to false

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export const GameConfig: Phaser.Types.Core.GameConfig = {
     default: 'arcade',
     arcade: {
       gravity: { y: 0 },
-      debug: true
+      debug: false
     }
   },
   scale: {
